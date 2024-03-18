8c2511f19fbdfc9c3b93470e0de68d651502786b
s0177158 <elisavetastyrchak@gmail.com>
2024-03-18 20:02:01 +0300

    8

--- db.php
+++ db.php
@@ -3,14 +3,14 @@
 $user = 'u67293'; // Заменить на ваш логин uXXXXX
 $pass = '3126725'; // Заменить на пароль, такой же, как от SSH
 $db = new PDO(
-    'mysql:host=localhost;dbname=form',
+    'mysql:host=localhost;dbname=forms1',
     $user, 
     $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
 
 try {
-    $stmt = $db->prepare("INSERT INTO form (name) VALUES (:fio)");
+    $stmt = $db->prepare("INSERT INTO forms1 (name) VALUES (:fio)");
     $fio - $_POST['fio'];
 }
 catch(PDOException $e){
